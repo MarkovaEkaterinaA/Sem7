75de0ad1eb80846ffc7e36a205350a5268cc25cf
MarkovaEkaterinaA <159148652+MarkovaEkaterinaA@users.noreply.github.com>
2024-03-13 13:26:08 +0300

    Add files via upload

---
+++ Sem7HT/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задайте произвольный массив. Выведете его элементы, 
+//начиная с конца. 
+//Использовать рекурсию, не использовать циклы.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Произвольный массив
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        // Вывод элементов массива, начиная с конца
+        PrintArrayReverse(array, array.Length - 1);
+
+        Console.ReadLine();
+    }
+
+    static void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.WriteLine(arr[index]);
+            PrintArrayReverse(arr, index - 1);
+        }
+    }
+}
